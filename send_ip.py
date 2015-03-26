d1f00eb8bb222a24caad37af5273260ef18c0129
duchenpaul <duchenpaul@gmail.com>
2015-03-26 08:32:10 +0800

    Update send_ip.py

--- send_ip.py
+++ send_ip.py
@@ -27,7 +27,7 @@ password = '1qazxsw2'
 
 output_date = subprocess.Popen(['date |cut -d " " -f 2-10| sed \'s/ CST.*//g\''], stdout=subprocess.PIPE, shell=True).communicate()[0]
 output_temp = subprocess.Popen(['/opt/vc/bin/vcgencmd measure_temp | cut -b 6-11'], stdout=subprocess.PIPE, shell=True).communicate()[0]    
-output_ip = subprocess.Popen(['curl -o - http://www.cpanel.net/showip.cgi'], stdout=subprocess.PIPE, shell=True).communicate()[0]
+output_ip = subprocess.Popen(['curl -o - http://www.cpanel.com/showip.cgi'], stdout=subprocess.PIPE, shell=True).communicate()[0]
 output_ESSID = subprocess.Popen(['iwconfig wlan0|grep ESSID|cut -d " " -f 9|sed \'s/ESSID:"//g\'|sed \'s/"//g\''], stdout=subprocess.PIPE, shell=True).communicate()[0]
 output_inner_ip = subprocess.Popen(['echo $(hostname -I) || true'], stdout=subprocess.PIPE, shell=True).communicate()[0]
     
