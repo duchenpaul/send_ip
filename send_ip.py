8c0cc30274d3b4de36dc4ec212516eff8ce5980a
duchenpaul <duchenpaul@gmail.com>
2015-11-25 00:36:38 +0800

    Update for more info

--- send_ip.py
+++ send_ip.py
@@ -23,7 +23,7 @@ toaddr = 'qq859755014@126.com'
 
 # Googlemail login details
 username = 'qq859755014@126.com'
-password = '1qazxsw2'
+password = 'lrvxmouxswxizkgq'
 
 output_date = subprocess.Popen(['date |cut -d " " -f 2-10| sed \'s/ CST.*//g\''], stdout=subprocess.PIPE, shell=True).communicate()[0]
 output_temp = subprocess.Popen(['/opt/vc/bin/vcgencmd measure_temp | cut -b 6-11'], stdout=subprocess.PIPE, shell=True).communicate()[0]    
@@ -36,7 +36,14 @@ send_temp = "Temperature: %s" % (output_temp)
 send_ip = "External IP: %s" % (output_ip)
 send_ESSID = "ESSID: %s" % (output_ESSID)
 send_inner_ip = "LAN IP: %s" % (output_inner_ip)
-yeelink = "For more information, visit: \nhttp://www.yeelink.net/devices/15028\nhttp://www.lewei50.com/u/g/8703\n"
+yeelink = "For more information, visit: \n\
+http://www.yeelink.net/devices/15028\n\
+http://www.lewei50.com/u/g/8703\n\
+http://www.wsncloud.com/device/56514e46e4b0932584ded5e4\n\
+https://dashboard.devicehub.net/project/6062/device/f41c2cd1-a05e-462e-b674-4b00b8addc4c/public/c6e6006a-3078-43f2-8f67-0f6d35d6d6a8\n\
+\nXiaoMi Router:\nhttp://www.wsncloud.com/device/56541303e4b00415c4381c6a\
+https://dashboard.devicehub.net/project/6062/device/6210d047-846e-42c8-8366-6d0da7275666/public/c6e6006a-3078-43f2-8f67-0f6d35d6d6a8\n\
+"
 
 BODY = string.join((
 "From: %s" % fromaddr,
